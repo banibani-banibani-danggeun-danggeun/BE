f5badb59ce021d9717561a9e55e0424784134316
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2022-12-26 16:44:08 +0900

    kakao_OAuth 1차 수정
    ㅇ

--- bannybannycarrotcarrot/src/main/java/com/week7/bannybannycarrotcarrot/entity/User.java
+++ bannybannycarrotcarrot/src/main/java/com/week7/bannybannycarrotcarrot/entity/User.java
@@ -25,10 +25,29 @@ public class User extends TimeStamped{
     @Column
     private UserRole userRole;
 
+    private Long kakaoId;  // kakao OAuth 를 위해 추가 -종열
+
+    private String email;
+
     @Builder
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.userRole = UserRole.USER;
     }
+
+
+    // kakao OAuth 를 위해 추가 -종열
+    public User(String username, Long kakaoId, String password, String email) {
+        this.username = username;
+        this.kakaoId = kakaoId;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
+    }
+
 }
